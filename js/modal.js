407ff0b4c8235057798b98cdb45f66410216f1e6
racuelcer <cervr413@newschool.edu>
2016-10-12 21:04:44 -0400

    commit

--- js/modal.js
+++ js/modal.js
@@ -20,14 +20,14 @@ function closeReg(evnt){
   let c = this.parentNode.parentNode.parentNode.querySelector('.ventana');
   c.style.display = "none";
 }
-let modalBack = document.querySelectorAll('.container.ventana');
+let modalBack = document.querySelectorAll('.container .ventana');
 
 for(let i=0; i < modalBack.length; i++){
   modalBack[i].addEventListener("click", closeWindow, false);
 }
 
 function closeWindow(evnt) {
-  let p = this.parentNode.parentNode.parentNode.querySelector('.ventana');
+  let p = this.parentNode.parentNode.querySelector('.ventana');
   p.style.display = "none";
 }
 
@@ -44,38 +44,3 @@ function closeModal(evnt){
 }
 
 
-function toggleFullScreen() {
-  if ((document.fullScreenElement && document.fullScreenElement !== null) || 
-      (!document.mozFullScreen && !document.webkitIsFullScreen)) {              
-    if (document.documentElement.requestFullScreen) {
-      document.documentElement.requestFullScreen();
-    } else if (document.documentElement.mozRequestFullScreen) {
-      document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullScreen) {
-      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-    }
-  } else {
-    if (document.cancelFullScreen) {
-      document.cancelFullScreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitCancelFullScreen) {
-      document.webkitCancelFullScreen();
-    }
-  }
-}
-
-document.addEventListener("keydown", function(evnt) {
-  if (evnt.keyCode == 13) {
-    toggleFullScreen();
-  }
-   if (evnt.keyCode == 32) {
-    if (document.cancelFullScreen) {
-      document.cancelFullScreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitCancelFullScreen) {
-      document.webkitCancelFullScreen();
-    }
- }
-}, false);
